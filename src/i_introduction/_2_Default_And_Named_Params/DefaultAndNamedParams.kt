e0743b719af1d971938ca5a01167c0df57319388
griffio <griffio@users.noreply.github.com>
2015-05-22 07:05:25 +0100

    2

--- src/i_introduction/_2_Default_And_Named_Params/DefaultAndNamedParams.kt
+++ src/i_introduction/_2_Default_And_Named_Params/DefaultAndNamedParams.kt
@@ -2,6 +2,7 @@ package i_introduction._2_Default_And_Named_Params
 
 import util.TODO
 import i_introduction._1_Functions.task1
+import java.util.Locale
 
 fun bar(i: Int, s: String = "", b: Boolean = true) {}
 
@@ -18,12 +19,12 @@ fun todoTask2_1() = TODO(
     """,
     references = { name: String -> JavaCode2().foo(name); foo(name) })
 
-fun foo(name: String): String = todoTask2_1()
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false): String =
+    (if (toUpperCase) name.toUpperCase(Locale.ROOT) else name) + number
 
 fun task2_1(): String {
-    todoTask2_1()
-//    return (foo("a") +
-//            foo("b", number = 1) +
-//            foo("c", toUpperCase = true) +
-//            foo(name = "d", number = 2, toUpperCase = true))
+    return (foo("a") +
+            foo("b", number = 1) +
+            foo("c", toUpperCase = true) +
+            foo(name = "d", number = 2, toUpperCase = true))
 }
